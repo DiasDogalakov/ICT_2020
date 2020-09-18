b7d631d4bf6e7115789f8f245d0226817f1d00c2
DiasDogalakov <60038731+DiasDogalakov@users.noreply.github.com>
2020-09-18 21:57:26 +0600

    Create 8.py

---
+++ task 1/8.py
@@ -0,0 +1,6 @@
+widgets = int(input("Введите количество widgets: "))
+gizmos = int(input("Введите количество gizmos: "))
+wOFwidget = widgets * 75
+wOFgizmo = gizmos * 112
+total = wOFwidget + wOFgizmo
+print(str(total) + " grams")
