1f71b175d78d90225a3b2af8d5685ea7937c6211
DiasDogalakov <60038731+DiasDogalakov@users.noreply.github.com>
2020-09-19 00:02:04 +0600

    Create 27.py

---
+++ task 1/27.py
@@ -0,0 +1,6 @@
+h_m = float(input("height(m): "))
+w_k = float(input("weight(kg): "))
+h_i = float(input("height(inches): "))
+w_p = float(input("weight(pounds): "))
+print(str((w_p / (h_i)**2)*703))
+print(str(w_k / (h_m)**2))
