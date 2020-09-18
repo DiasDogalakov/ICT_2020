9feb7f22ae3792bc5cb673ef74aca5748b2ee615
DiasDogalakov <60038731+DiasDogalakov@users.noreply.github.com>
2020-09-18 22:18:56 +0600

    Create 10.py

---
+++ task 1/10.py
@@ -0,0 +1,11 @@
+import math
+a = int(input("a: "))
+b = int(input("b: "))
+
+print("Sum: " + str(a + b))
+print("Difference: " + str(a - b))
+print("Product: " + str(a * b))
+print("Quotient: " + str(a // b))
+print("Remainder: " + str(a % b))
+print("Result of log10 a: " + str(math.log10(a)))
+print("Result a^b: " + str(a ** b))
